8355a4a92bb3e26f7b68a8b6a5d9382f77cef373
MzAngel0 <miguel.martinez.isaza2007@gmail.com>
2026-05-26 16:35:26 -0500

    Agregamos la clase del mago de hielo con la habilidad del turno congelado

---
+++ src/main/java/org/example/MagoHielo.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class MagoHielo extends Mago{
+
+    private int turnoCongelado;
+
+    public MagoHielo(String nombre){
+        super(nombre, 110, 75, 25, "Hielo");
+        this.turnoCongelado = 0;
+    }
+
+    @Override
+    public String lanzarHechizo(Mago rival) {
+        rival.setVida(rival.getVida() - 30);
+        // 40% de probabilidad de congelar
+        if (Math.random() < 0.4) {
+            turnoCongelado = 1;
+        }
+        return getNombre() + " lanza Ventisca! (-30 vida)";
+    }
+    public int getTurnoCongelado() { return turnoCongelado; }
+
+    public void setTurnoCongelado(int t) { this.turnoCongelado = t; }
+}
